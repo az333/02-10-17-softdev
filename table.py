900cf765c6bec6676ee107ea1dfc4e3a72a00121
Xing Tao Shi <xshi@stuy.edu>
2017-10-02 13:58:38 -0400

    class sdtufF

--- table.py
+++ table.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 
 my_app = Flask(__name__)
 
-@my_app.route('/')
+@my_app.route('/', methods = ["POST", "GET"])
 def root():
+    print request
+    print request.headers
+    print request.method
+    print request.range
+    print "args"
+    print request.args
+    print request.args['input']
     return render_template('form.html', title = "Form") 
 
 
